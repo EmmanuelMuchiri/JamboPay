c9af48fed89f3dbb22596e8fe15db2c02c0f5cfc
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-10-16 16:38:57 +0300

    changes

--- Jambopay/settings.py
+++ Jambopay/settings.py
@@ -211,11 +211,11 @@ BOOTSTRAP4 = {
 
 
 # Email configurations remember to install python-decouple
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # EMAIL_HOST='smtp.gmail.com'
 # EMAIL_USE_TLS=True
